b55d720df2f99b311dcf1176e9ff1842068e9204
alya0124 <23301542@uttt.edu.mx>
2024-11-12 20:08:56 -0600

    Despliegue 2

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def editar_escaneo(id):
         return jsonify({"message": "Error al actualizar el escaneo"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
